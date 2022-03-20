9298bd96ed4374149abf0f3160690a65d8b05a2b
Ankit Tawale <ankit.tawale@perficient.com>
2022-03-20 15:23:28 +0530

    Add project files.

---
+++ Controllers/EnrollmentController.cs
@@ -0,0 +1,50 @@
+﻿using Login_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login_Project.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+        public string value = "";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Enroll e)
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                Enroll er = new Enroll();
+                using (SqlConnection con = new SqlConnection("Data Source=ZIL1038\\SQLDEV16;Integrated Security=true;Initial Catalog=Training"))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SP_EnrollDetail", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@FirstName", e.FirstName);
+                        cmd.Parameters.AddWithValue("@LastName", e.LastName);
+                        cmd.Parameters.AddWithValue("@Password", e.Password);
+                        cmd.Parameters.AddWithValue("@Gender", e.Gender);
+                        cmd.Parameters.AddWithValue("@Email", e.Email);
+                        cmd.Parameters.AddWithValue("@Phone", e.PhoneNumber);
+                        cmd.Parameters.AddWithValue("@SecurityAnwser", e.SecurityAnwser);
+                        cmd.Parameters.AddWithValue("@status", "INSERT");
+                        con.Open();
+                        ViewData["result"] = cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
